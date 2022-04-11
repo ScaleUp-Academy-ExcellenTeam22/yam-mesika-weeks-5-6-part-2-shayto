9a0e48cc9239bd34ae533b24ff17f68e16e251ac
Shay <shaindel720@gmail.com>
2022-04-11 19:23:42 +0300

    Updated_Perfect.

--- perfect_numbers.py
+++ perfect_numbers.py
@@ -9,13 +9,12 @@ def all_perfect_numbers():
     while True:
         if sum(number for number in range(1, up_to) if not up_to % number) == up_to:
             yield up_to
-        up_to = up_to + 1
+        up_to += 1
 
 
 if __name__ == '__main__':
     all_perfect_numbers_generator = all_perfect_numbers()
-    print(next(all_perfect_numbers_generator))
-    print(next(all_perfect_numbers_generator))
-    print(next(all_perfect_numbers_generator))
-    print(next(all_perfect_numbers_generator))
+    for number in range(4):
+        print(next(all_perfect_numbers_generator))
+
     # After the 4th call the computer works really hard.
